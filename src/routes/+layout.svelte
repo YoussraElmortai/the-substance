d5cd56cf931d94cffabf70b3a5422e873898e7eb
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-06-03 10:12:21 +0200

    styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,3 +8,24 @@
     And people always ask for something new.
 Renewal is inevitable
 </footer>
+
+<style>
+    header{
+        padding: 1rem;
+        border-radius: 1.5rem;
+        border: solid;
+    }
+
+    main{
+        display: flex;
+        flex-flow: column;
+        gap: 0.5rem;
+    }
+
+    footer{
+        font-weight: bold;
+        width: 100%;
+        display: flex;
+        justify-content: end;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,22 @@
 <script></script>
 
-Have you ever dreamt of a better version of yourself?
-Younger.
-More beautiful.
-More perfect.
-One single injection unlocks your DNA.
-Starting a new cellular division.
-That will release another version of yourself.
-This is… The Substance.
-You are the matrix.
-Everything comes from you.
-And everything is you.
-This is simply a better version of yourself.
-You just have to share.
-One week for one.
-And one week for the other.
-A perfect balance of seven days each.
-The one and only thing not to forget.
-You are one.
+<section class="hero">
+	<h1>Have you ever dreamt of a better version of yourself? Younger.</h1>
+</section>
+<section class="about">
+	<h2>
+		One single injection unlocks your DNA. Starting a new cellular division. That will release
+		another version of yourself.
+	</h2>
+</section>
+<section class="rule">
+	<h2>A perfect balance of seven days each. The one and only thing not to forget. You are one.</h2>
+</section>
+
 <style>
-    
+    section{
+        height: 90vh;
+        background: grey;
+        border-radius: 1.5rem;
+    }
 </style>
